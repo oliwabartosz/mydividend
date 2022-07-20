6ec88caf03ce019058a7ee4ab1fa126c69e9c7ed
Bartosz O <oliwa.bartosz@gmail.com>
2022-07-20 14:40:51 +0200

    Added (started) app: dashboard

--- mydividend/settings.py
+++ mydividend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import json
 
-with open ('../config.json') as config_file:
+with open ('config.json') as config_file:
     config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
