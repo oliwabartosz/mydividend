59180ba26f9a18538269aae80c678ec9e929be49
Bartosz O <oliwa.bartosz@gmail.com>
2022-07-24 15:01:21 +0200

    added method POST to: 1. navbar search inputbox in base.html, 2. views.py.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+import requests
+import json
+
+with open ('config.json') as config_file:
+    config = json.load(config_file)
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+
+    if request.method == 'POST':
+        ticker = request.POST.get('ticker')
+        api_request = requests.get(f"{config['API_LINK']}{ticker}{config['API_KEY']}")
+
+        try:
+            api_data = json.loads(api_request.content)
+        except Exception as e:
+            api_data = False
+            
+        return render(request, 'home.html', {'api_data': api_data,})
+    else:
+        return render(request, 'home.html')
